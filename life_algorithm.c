ed31770b463dd46ee64871124d929d874b9aa070
happysmash27 <happysmash27@protonmail.com>
2025-02-23 20:47:32 -0800

    Prepare to split into functions

--- life_algorithm.c
+++ life_algorithm.c
@@ -1,22 +1,24 @@
-if (is_meeting){
-  do_meeting();
- }
-if (is_work_day()){
-  if (is_at_work){
-    do_work_tasks();
+void main(){
+  if (is_meeting){
+    do_meeting();
   }
-  else if (time_to_work <= 2h){
-    prepare_to_leave_to_work();
+  if (is_work_day()){
+    if (is_at_work){
+      do_work_tasks();
+    }
+    else if (time_to_work <= 2h){
+      prepare_to_leave_to_work();
+    }
+  } else if (time_to_work < 12h){
+    wash_prep_drinks();
+    go_to_bed();
+  } else if (is_at_home){
+    if (is_enough_energy() && is_enough_compute()){
+      work_on_takuno_render();
+    } else if (is_enough_energy() && is_chore(chore_list)) {
+      do_chores(chore_list);
+    } else {
+      work_on_portage_update();
+    }
   }
- } else if (time_to_work < 12h){
-  wash_prep_drinks();
-  go_to_bed();
- } else if (is_at_home){
-  if (is_enough_energy() && is_enough_compute()){
-    work_on_takuno_render();
-  } else if (is_enough_energy() && is_chore(chore_list)) {
-    do_chores(chore_list);
-  } else {
-    work_on_portage_update();
-  }
- }
+}
